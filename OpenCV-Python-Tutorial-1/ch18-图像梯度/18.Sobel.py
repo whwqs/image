dd1e01c75a5d7d420c2ceaf26a7d17c790fd77d2
whwqs <whwqs@hotmail.com>
2019-03-03 20:56:08 +0800

    学习梯度

--- OpenCV-Python-Tutorial-1/ch18-图像梯度/18.Sobel.py
+++ OpenCV-Python-Tutorial-1/ch18-图像梯度/18.Sobel.py
@@ -23,11 +23,14 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('../data/sudoku.jpg', 0)
+img = cv2.imread('sudoku.jpg', 0)
 # cv2.CV_64F 出图像的深度 数据类型 可以使用 -1, 与原图像保持一致 np.uint8
 laplacian = cv2.Laplacian(img, cv2.CV_64F)
 # 参数 1,0 为只在 x 方向求一 导数 最大可以求 2 导数。
 sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=5)
+
+print(sobelx)
+
 # 参数 0,1 为只在 y 方向求一 导数 最大可以求 2 导数。
 sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=5)
 
