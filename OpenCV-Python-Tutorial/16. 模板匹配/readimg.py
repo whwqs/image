ae3daa0c84cc5cb6efb65194fc12fd91d2d29564
whwqs <whwqs@hotmail.com>
2019-03-29 21:12:02 +0800

    模板

--- OpenCV-Python-Tutorial/16. 模板匹配/readimg.py
+++ OpenCV-Python-Tutorial/16. 模板匹配/readimg.py
@@ -40,11 +40,11 @@ def getCode(imgpath,name):
         try:
             
             t_ = eval("t["+tloc[i]+"]") 
-            #print(i)#定位出错代码
+            
             t_ = cv2.resize(t_, None, fx=fx[i], fy=fy[i], interpolation=cv2.INTER_CUBIC)
-            #print(i)#定位出错代码
+            
         except:
-            return "模板出错，请检查配置文件"        
+            return "模板出错，请检查配置文件:"+ str(i)       
                 
         res = cv2.matchTemplate(part, t_ , cv2.TM_CCOEFF_NORMED)        
         loc = np.where(res >= threshold[i]) 
@@ -68,11 +68,108 @@ def getCode(imgpath,name):
         code += str(p["i"])
     return code
 
+def getCode2(imgpath,name):      
+    setting = cfg.get("t_"+name)
+    codeset = setting["code"] 
+    t = cv2.imread(codeset["tfile"],0)    
+    tloc = codeset["tloc"]
+    fx = codeset["fx"]
+    fy = codeset["fy"]
+    x,y,w,h = codeset["loc"]
+    threshold = codeset["threshold"]
+    image = cv2.imread(imgpath,0)
+    part = image[y:y+h,x:x+w]    
+    part_reginos = get_number_region(part)
+    code = ""
+    templateDic = {}
+    
+    for region in part_reginos:    
+        max = -100
+        number = ""
+        for i in range(10):
+            key = "t"+str(i)
+            if key in templateDic:
+                t_ = templateDic[key]
+            else:
+                try:            
+                    t_ = eval("t["+tloc[i]+"]")             
+                    t_ = cv2.resize(t_, None, fx=fx[i], fy=fy[i], interpolation=cv2.INTER_CUBIC)  
+                    templateDic[key] = t_
+                except:
+                    return "模板出错，请检查配置文件:"+ str(i)  
+            res = cv2.matchTemplate(region, t_ , cv2.TM_CCOEFF_NORMED)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+            if max_val>max:
+                number = str(i)
+                max = max_val
+        code += number
+    return code
+
+def get_number_region(img):
+    
+    img = cv2.blur(img, (3, 3))
+    (_, thresh) = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)
+    # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))  # 形态学处理:定义矩形结构
+    closed = cv2.erode(thresh, None, iterations=1)  # 闭运算：迭代5次
+
+    height, width = closed.shape[:2]
+    z = [0] * height
+    # 垂直投影：统计并存储每一列的黑点数
+    lst_x = list()
+    for x in range(0, width):
+        for y in range(0, height):
+            if closed[y, x] == 0:
+                lst_x.append(x)
+            else:
+                continue
+
+    # 水平投影  #统计每一行的黑点数
+    a = 0
+    emptyImage1 = np.zeros((height, width), np.uint8)
+    for y in range(0, height):
+        for x in range(0, width):
+            if closed[y, x] == 0:
+                a = a + 1
+            else:
+                continue
+        z[y] = a
+        a = 0
+
+    # 绘制水平投影图
+    for y in range(0, height):
+        for x in range(0, z[y]):
+            b = 255
+            emptyImage1[y, x] = b
+
+    # 获取y轴坐标
+    lst_y = list()
+    start = 0
+    for y in range(0, height):
+        if emptyImage1[y, 0] == 255:
+            if not start:
+                start = y
+        else:
+            if emptyImage1[y, 0] == 0:
+                if start:
+                    lst_y.append((start, y))
+                    start = 0
+            continue
+
+    #print(lst_x[0], lst_x[-1])
+    #print(lst_y)
+    region = []
+    for i in lst_y:
+        corp_img = img[i[0]:i[1], 0:width]
+        region.append(corp_img)
+        #cv2.imwrite("{}.jpg".format(lst_y.index(i)), corp_img)
+    
+    return region
+
 if __name__== '__main__':
-    functype = sys.argv[1]
-    imgpath = sys.argv[2]
-    name = sys.argv[3]    
+    functype = "code"#sys.argv[1]
+    imgpath = "20190329_150621.bmp"#sys.argv[2]
+    name = "黄鹤楼"#sys.argv[3]    
     if functype == "barcode":
         print(getBarcode(imgpath,name)[0])
     else:        
-        print(getCode(imgpath,name))
+        print(getCode2(imgpath,name))
