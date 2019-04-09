96682e7f3a778e0be7a67f7c7ecf7d554fc397a2
whwqs <whwqs@hotmail.com>
2019-04-09 19:14:19 +0800

    配置加上图像最少像素数量

--- OpenCV-Python-Tutorial/16. 模板匹配/CodeRecognitionAndAlarm/readimg.py
+++ OpenCV-Python-Tutorial/16. 模板匹配/CodeRecognitionAndAlarm/readimg.py
@@ -32,16 +32,18 @@ def getCode(imgpath,name):
     loc = codeset["loc"]
     resize = codeset["resize"]
     blursize = codeset["blursize"]
+    tminpx = codeset["tminpx"]
+    minpx = codeset["minpx"]
 
     img1 = cv2.imread(tfile,0)
     temp1 = template(img1,tloc,tv,blursize)
-    temp1.split(20)
+    temp1.split(tminpx)
     temp1.check()
-    temp1.show(3,True)
+    #temp1.show(3,True)
 
     img2 = cv2.imread(imgpath,0)    
     temp2 = template(img2,loc,[],blursize)
-    temp2.split(20)
+    temp2.split(minpx)
     #temp2.show(14,False)
     code = ""    
     for p in temp2.imglist:  
